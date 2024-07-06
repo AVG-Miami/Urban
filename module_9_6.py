2d297ecfb9fffea775f9e957923fca09551a76c5
AVG-Miami <166794127+AVG-Miami@users.noreply.github.com>
2024-07-06 23:31:09 +0800

    Домашнее задание по теме "Генераторы"
    Домашнее задание по теме "Генераторы"

---
+++ module_9_6.py
@@ -0,0 +1,12 @@
+def all_variants(text):
+    i=0
+    while i!=len(text):
+        yield text[i]
+        i+=1
+    yield text[0]+text[1]
+    yield text[1]+text[2]
+    yield text[0]+text[1]+text[2]
+
+a = all_variants("abc")
+for i in a:
+    print(i)
