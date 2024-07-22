4815e1863ce85be0390b7a8c1da35bae620d4b1a
AVG-Miami <166794127+AVG-Miami@users.noreply.github.com>
2024-07-23 06:49:08 +0800

    Update module_10.2.2.py
    Домашнее задание по теме "Многопроцессное программирование"

--- module_10.2.2.py
+++ module_10.2.2.py
@@ -1,12 +1,15 @@
 import multiprocessing
-from time import sleep
 
 
 class WarehouseManager:
+
     data = {}
 
-    def process_request(self, requests):
+    def __init__(self):
+#       self.data={}
+       print("e")
 
+    def process_request(self, requests):
         if requests[1] == "receipt":
             if requests[0] in self.data:
                 self.data.update( {requests[0]: self.data[requests[0]] + requests[2]} )
@@ -17,34 +20,33 @@ class WarehouseManager:
                 print( "поступил новый {} в количестве {}  **** состояние склада {}".format( requests[0], requests[2],
                                                                                              self.data ) )
         else:
+
             if requests[0] in self.data:
-                self.data.update( {requests[0]: self.data[requests[0]] - requests[2]} )
-                print( "отгрузили {} в количестве {}  **** состояние склада {}".format( requests[0], requests[2],
-                                                                                        self.data ) )
+
+                if self.data[requests[0]]>0:
+                    self.data.update( {requests[0]: self.data[requests[0]] - requests[2]} )
+                    print( "отгрузили {} в количестве {}  **** состояние склада {}".format( requests[0], requests[2],
+                                                                                       self.data ) )
 
     def run(self, requests):
-        if __name__ == '__main__':
-            with multiprocessing.Pool( processes=1 ) as pool:
-                pool.map( self.process_request, requests )
+        with multiprocessing.Pool( processes=4) as pool:
+            pool.map(self.process_request, requests )
+
 
 
 # Создаем менеджера склада
-manager = WarehouseManager()
-
+print(__name__)
+if __name__ == '__main__':
+    manager = WarehouseManager()
 # Множество запросов на изменение данных о складских запасах
-requests = [
-    ("product1", "receipt", 100),
-    ("product2", "receipt", 150),
-    ("product1", "shipment", 30),
-    ("product3", "receipt", 200),
-    ("product2", "shipment", 50),
-    ("product2", "receipt", 50)
-]
-
+    requests = [
+        ("product1", "receipt", 100),
+        ("product2", "receipt", 150),
+        ("product1", "shipment", 30),
+        ("product3", "receipt", 200),
+        ("product2", "shipment", 50)
+    ]
 # Запускаем обработку запросов
-manager.run( requests )
-
+    manager.run( requests)
 # Выводим обновленные данные о складских запасах
-
-
-print( "состояние склада", manager.data )
+    print( "состояние склада", manager.data )
