0f4fb58d7236e01ed5c2666a7990f26d605729d8
AVG-Miami <166794127+AVG-Miami@users.noreply.github.com>
2024-07-23 09:27:44 +0800

    Update module_10.2.2.py
    Домашнее задание по теме "Многопроцессное программирование"

--- module_10.2.2.py
+++ module_10.2.2.py
@@ -1,13 +1,10 @@
 import multiprocessing
 
-
 class WarehouseManager:
-
     data = {}
-
     def __init__(self):
-#       self.data={}
-       print("e")
+        #        self.data={}
+        print( "init" )
 
     def process_request(self, requests):
         if requests[1] == "receipt":
@@ -20,33 +17,28 @@ class WarehouseManager:
                 print( "поступил новый {} в количестве {}  **** состояние склада {}".format( requests[0], requests[2],
                                                                                              self.data ) )
         else:
-
             if requests[0] in self.data:
-
-                if self.data[requests[0]]>0:
-                    self.data.update( {requests[0]: self.data[requests[0]] - requests[2]} )
-                    print( "отгрузили {} в количестве {}  **** состояние склада {}".format( requests[0], requests[2],
-                                                                                       self.data ) )
+                self.data.update( {requests[0]: self.data[requests[0]] - requests[2]} )
+                print( "отгрузили {} в количестве {}  **** состояние склада {}".format( requests[0], requests[2],
+                                                                                        self.data ) )
 
     def run(self, requests):
-        with multiprocessing.Pool( processes=4) as pool:
-            pool.map(self.process_request, requests )
-
+        with multiprocessing.Pool( processes=5 ) as pool:
+            pool.map( self.process_request, requests )
 
 
 # Создаем менеджера склада
-print(__name__)
 if __name__ == '__main__':
     manager = WarehouseManager()
-# Множество запросов на изменение данных о складских запасах
+    # Множество запросов на изменение данных о складских запасах
     requests = [
         ("product1", "receipt", 100),
         ("product2", "receipt", 150),
         ("product1", "shipment", 30),
         ("product3", "receipt", 200),
-        ("product2", "shipment", 50)
+        ("product2", "shipment", 50),
     ]
-# Запускаем обработку запросов
-    manager.run( requests)
-# Выводим обновленные данные о складских запасах
+    # Запускаем обработку запросов
+    manager.run( requests )
+    # Выводим обновленные данные о складских запасах
     print( "состояние склада", manager.data )
