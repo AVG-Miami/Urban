ceedf87dec1ca6d00a09121f052f7ed7145fa7a0
AVG-Miami <166794127+AVG-Miami@users.noreply.github.com>
2024-08-09 13:37:49 +0800

    Update suite_12_3.py

--- suite_12_3.py
+++ suite_12_3.py
@@ -1,10 +1,10 @@
 import unittest
-import dz_mod_12_2
-import dz_mod_12_1
+import dz_mod_12_2_1
+import dz_mod_12_1_1
 
 testST=unittest.TestSuite()
-testST.addTest(unittest.TestLoader().loadTestsFromTestCase(dz_mod_12_1.RunnerTest))
-testST.addTest(unittest.TestLoader().loadTestsFromTestCase(dz_mod_12_2.TournamentTest))
+testST.addTest(unittest.TestLoader().loadTestsFromTestCase(dz_mod_12_1_1.RunnerTest))
+testST.addTest(unittest.TestLoader().loadTestsFromTestCase(dz_mod_12_2_1.TournamentTest))
 
 
 runner = unittest.TextTestRunner(verbosity=2)
