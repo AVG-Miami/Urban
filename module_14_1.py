78ac802627ed22a0ac3582c6329a3b0cda624682
AVG-Miami <166794127+AVG-Miami@users.noreply.github.com>
2024-08-21 12:25:30 +0800

    ДЗ "Создание БД, добавление, выбор и удаление элементов."
    Домашнее задание по теме "Создание БД, добавление, выбор и удаление элементов."

---
+++ module_14_1.py
@@ -0,0 +1,42 @@
+'''
+Домашнее задание по теме "Создание БД, добавление, выбор и удаление элементов."
+'''
+
+import sqlite3
+import random
+from pprint import pprint
+
+connnection = sqlite3.connect( 'not_telegram.db ' )
+cursor = connnection.cursor()
+cursor.execute( '''
+               CREATE TABLE IF NOT EXISTS Users(
+               id INTEGER PRIMARY KEY,
+               username TEXT NOT NULL,
+               email TEXT NOT NULL,
+               age INTEGER,
+               balance INTEGER NOT NULL 
+               )
+               ''' )
+# Удаляем все записи
+cursor.execute( "DELETE FROM Users" )
+
+# Заполните её 10 записями:
+for i in range( 1, 11 ):
+    cursor.execute( " INSERT INTO Users (username, email, age, balance) VALUES (?,?,?,?)",
+                    (f"User{i}", f"example{i}@gmail.com", 10 * i, 1000) )
+
+# Обновите balance у каждой 2ой записи начиная с 1ой на 500:
+cursor.execute( "UPDATE Users SET balance = ? WHERE ID%2", (500,) )
+
+# Удалите каждую 3ую запись в таблице начиная с 1ой:
+cursor.execute( "DELETE FROM Users WHERE NOT (ID-1)%3" )
+
+# Сделайте выборку всех записей при помощи fetchall(), где возраст не равен 60 и выведите их в консоль в следующем формате (без id):
+# Имя: <username> | Почта: <email> | Возраст: <age> | Баланс: <balance>
+cursor.execute( "SELECT username, email, age, balance FROM Users WHERE age<>60" )
+users = cursor.fetchall()
+for user in users:
+    print( user[0], "|", user[1], "|", user[2], "|", user[3] )
+
+connnection.commit()
+connnection.close()
