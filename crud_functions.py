9c8f78ce9e7d70c1c23291cc2d3876ed5c55232a
AVG-Miami <166794127+AVG-Miami@users.noreply.github.com>
2024-08-23 11:36:36 +0800

    ДЗ "Написание примитивной ORM"
    Домашнее задание по теме "Написание примитивной ORM"

--- crud_functions.py
+++ crud_functions.py
@@ -13,6 +13,15 @@ def initiate_db():
                    price INTEGER NOT NULL
                    )
                    ''')
+    cursor.execute('''
+                       CREATE TABLE IF NOT EXISTS Users(
+                       id INTEGER PRIMARY KEY,
+                       username TEXT NOT NULL,
+                       email TEXT NOT NULL,
+                       age INTEGER NOT NULL,
+                       balance INTEGER NOT NULL
+                       )
+                       ''')
 
     cursor.execute(" INSERT INTO Products (title, description, price) VALUES (?,?,?)", ("Продукт_1", "Описание_1", 100))
     cursor.execute(" INSERT INTO Products (title, description, price) VALUES (?,?,?)", ("Продукт_2", "Описание_2", 200))
@@ -20,6 +29,38 @@ def initiate_db():
     cursor.execute(" INSERT INTO Products (title, description, price) VALUES (?,?,?)", ("Продукт_4", "Описание_4", 400))
     connnection.commit()
     connnection.close()
+
+
+def is_included(username):
+    connnection = sqlite3.connect('db_product.db')
+    cursor = connnection.cursor()
+    sql = "SELECT * FROM Users WHERE username = ?"
+    print()
+    print("in function ", sql, username)
+    print()
+    cursor.execute(sql, (username,))
+
+    user = cursor.fetchone()
+    connnection.commit()
+    print()
+    print("Пользователь", user)
+    print()
+    if user == None:
+        return False
+    else:
+        return True
+    #    connnection.commit()
+    connnection.close()
+
+
+def add_user(username, email, age):
+    print('222222')
+    connnection = sqlite3.connect('db_product.db')
+    cursor = connnection.cursor()
+    cursor.execute("INSERT INTO Users (username, email, age,balance) VALUES (?,?,?,?)", (username, email, age, 1000))
+    connnection.commit()
+    connnection.close()
+
 
 def get_all_products():
     connnection = sqlite3.connect('db_product.db')
@@ -29,4 +70,14 @@ def get_all_products():
     connnection.commit()
     connnection.close()
 
-initiate_db()
+
+def get_all_users():
+    connnection = sqlite3.connect('db_product.db')
+    cursor = connnection.cursor()
+    cursor.execute("SELECT * FROM users")
+    return cursor.fetchall()
+    connnection.commit()
+    connnection.close()
+
+# initiate_db()
+# is_included("a")
