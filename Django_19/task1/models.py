b06d4f3c6b62c4d9db510d05f41cb7ae78cd129c
AVG-Miami <166794127+AVG-Miami@users.noreply.github.com>
2024-10-17 13:15:34 +0800

    ДЗ по теме "Модели баз данных в Django."
    Домашнее задание по теме "Модели баз данных в Django."

---
+++ Django_19/task1/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+class Buyer(models.Model):
+    name = models.CharField(max_length=200)
+    balance = models.DecimalField(decimal_places=2, max_digits=10)
+    age = models.IntegerField()
+
+class Game(models.Model):
+    title  = models.CharField(max_length=200)
+    cost  = models.DecimalField(decimal_places=2, max_digits=10)
+    size  = models.DecimalField(decimal_places=2, max_digits=10)
+    description = models.TextField()
+    age_limited = models.BooleanField(default=True)
+    buyer  = models.ManyToManyField(Buyer, related_name="buyer_relates")
+
