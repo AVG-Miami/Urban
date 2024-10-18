f2581413268d813f3122fbeaa3d36d9362321998
AVG-Miami <166794127+AVG-Miami@users.noreply.github.com>
2024-10-18 22:40:40 +0800

    Add files via upload

---
+++ django19_3/pythonProject19/Django_19/task1/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+
+class Buyer(models.Model):
+    name = models.CharField(max_length=200)
+    balance = models.DecimalField(decimal_places=2, max_digits=10)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+class Game(models.Model):
+    title  = models.CharField(max_length=200)
+    cost  = models.DecimalField(decimal_places=2, max_digits=10)
+    size  = models.DecimalField(decimal_places=2, max_digits=10)
+    description = models.TextField()
+    age_limited = models.BooleanField(default=True)
+    buyer  = models.ManyToManyField(Buyer, related_name="buyer_relates")
+
+    def __str__(self):
+        return self.title
